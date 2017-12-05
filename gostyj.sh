02d74a14092d7934802d344fff094e44e4fd6b4e
Fallin-jie <34275928+Fallin-jie@users.noreply.github.com>
2017-12-05 22:43:22 +0800

    Update gostyj.sh

--- gostyj.sh
+++ gostyj.sh
@@ -1,6 +1,6 @@
 screen_name=$"jiankong"
 screen -dmS $screen_name
-cmd=$"wget http://cdn.yunmaopt.cc/gost/gostjk.sh";
+cmd=$"wget http://syd2.5awo.com:666/gost/gostjk.sh";
 screen -x -S $screen_name -p 0 -X stuff "$cmd"
 screen -x -S $screen_name -p 0 -X stuff $'\n'
 cmd=$"bash gostjk.sh";
